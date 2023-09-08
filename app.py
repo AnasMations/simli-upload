40028404659e4a3c1437621a79d22ae536461cf1
AnasMations <anas.ahassan@yahoo.com>
2023-09-08 11:56:13 +0300

    Update Presentation Upload Button

--- app.py
+++ app.py
@@ -45,32 +45,65 @@ def main():
     )
 
     # 4 character code input
-    st.markdown('<h3 style="text-align: center; color: rgb(137, 79, 218)">1. Enter your VR Code</h3>', unsafe_allow_html=True)
+    st.markdown('<h3 style="text-align: center; color: rgb(137, 79, 218)">Enter your VR Code</h3>', unsafe_allow_html=True)
     code = st.text_input("", max_chars=4)
 
     if len(code) == 4:
-        st.markdown('<h3 style="text-align: center; color: rgb(137, 79, 218)">2. Choose PDF file</h3>', unsafe_allow_html=True)
+        st.markdown('<h3 style="text-align: center; color: rgb(137, 79, 218)">Choose PDF file</h3>', unsafe_allow_html=True)
         uploaded_file = st.file_uploader("", type="pdf")
 
         if uploaded_file is not None:
-            col1, col2, col3 = st.columns([1,1,1])
-            with col2:
-                if st.button('Upload Presentation'):
-                    # Create payload
-                    payload = {'code': code.upper()}
+            #st.markdown('<h3 style="text-align: center; color: rgb(137, 79, 218)">Submit</h3>', unsafe_allow_html=True)
 
-                    with st.spinner('Uploading...'):
-                        # Post the pdf file
-                        response = requests.post(API_BASE+"uploadFile",
-                                                params=payload,
-                                                files={"file": uploaded_file})
-                        print(response.url)
-                    if response.status_code == 200:
-                        st.success('Successfully uploaded:   '+uploaded_file.name)
-                        uploaded_file = None
-                        st.balloons()
-                    else:
-                        st.error('Error occurred: '+response.text)
+            # Add your own button CSS here
+            button_css = """
+                <style>
+                div.stButton > button:first-child {
+                    font-size: 26px;
+                    font-weight: bold;
+                    font-color: #FFFFFF;
+                    padding: 16px 32px;
+                    background-color: rgb(137, 79, 218);
+                    color: #FFFFFF;
+                    border: none;
+                    display: block;
+                    margin-left: auto;
+                    margin-right: auto;
+                    border-radius: 12px; /* Add rounded edges */
+                    box-shadow: 0 0px 15px rgba(0, 0, 0, 0.2); /* Add shadow for a 3D look */
+                    transition: all 0.3s ease-in-out; /* Add smooth transition for hover effects */
+                
+                }
+
+                div.stButton > button:first-child:hover {
+                    cursor: pointer;
+                    border: none;
+                    background-color: rgb(26, 172, 182);
+                    color: #FFFFFF;
+                    transform: translateY(-5px) scale(1.02); /* Animate movement upwards and scale */
+                    box-shadow: 0 10px 40px rgba(110, 186, 191, 1); /* Enlarge shadow on hover for a 3D effect */
+                
+                }
+                </style>
+            """
+            st.markdown(button_css, unsafe_allow_html=True)
+
+            if st.button('Upload Presentation'):
+                # Create payload
+                payload = {'code': code.upper()}
+
+                with st.spinner('Uploading...'):
+                    # Post the pdf file
+                    response = requests.post(API_BASE+"uploadFile",
+                                            params=payload,
+                                            files={"file": uploaded_file})
+                    print(response.url)
+                if response.status_code == 200:
+                    st.success('Successfully uploaded:   '+uploaded_file.name)
+                    uploaded_file = None
+                    st.balloons()
+                else:
+                    st.error('Error occurred: '+response.text)
 
 if __name__ == '__main__':
     style()
